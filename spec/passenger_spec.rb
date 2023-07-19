590c7c9fcd1a1dc598b51a04e0deca35bdf3d210
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-19 09:10:52 -0600

    Add initialize test and 'name' test

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -2,10 +2,18 @@ require "./lib/passenger"
 
 RSpec.describe Passenger do
   before(:each) do
-    @charlie = Passenger.new({"name" => Charlie, "age" => 18})
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
     @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
   end
+  describe "#initialize" do 
+    it "exists" do
+      expect(@charlie).to be_instance_of(Passenger)
+    end
 
-  
+    it "has a name" do 
+      expect(@charlie.name).to eq("Cnarlie")
+    end
+  end
+
 end
   
