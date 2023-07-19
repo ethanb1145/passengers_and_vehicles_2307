6f2843e3b686aaac738815987963e9ad63edc145
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-19 09:34:09 -0600

    Create attributes and their tests for the vehicle and pass them

---
+++ spec/vehicle_spec.rb
@@ -0,0 +1,25 @@
+require "./lib/vehicle"
+require "./lib/passenger"
+
+RSpec.describe Vehicle do
+  before(:each) do
+    @vehicle = Vehicle.new("2001", "Honda", "Civic") 
+  end
+  describe "#initialize" do 
+    it "exists" do
+      expect(@vehicle).to be_instance_of(Vehicle)
+    end
+
+    it "has a year" do 
+      expect(@vehicle.year).to eq("2001")
+    end
+
+    it "has a make" do 
+      expect(@vehicle.make).to eq("Honda")
+    end
+
+    it "has a model" do 
+      expect(@vehicle.model).to eq("Civic")
+    end
+  end
+end
